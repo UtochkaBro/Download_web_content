f8194d37e346d8a125621de2bfcdcd2db91ca07c
Vyacheslav Nikitin <space.face.ru@gmail.com>
2022-10-10 16:46:03 +0300

    #3 created logic function download image

--- app/src/main/java/com/example/downloadwebcontent/DownWebImg.java
+++ app/src/main/java/com/example/downloadwebcontent/DownWebImg.java
@@ -2,9 +2,19 @@ package com.example.downloadwebcontent;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.ExecutionException;
 
 public class DownWebImg extends AppCompatActivity {
 
@@ -19,8 +29,40 @@ public class DownWebImg extends AppCompatActivity {
     }
 
     public void onClickDownImage(View view){
-
+        DownloadImageTask task = new DownloadImageTask();
+        Bitmap bitmap = null;
+        try {
+            bitmap = task.execute(url).get();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        imageView.setImageBitmap(bitmap);
     }
 
+    private static class DownloadImageTask extends AsyncTask<String, Void, Bitmap>{
+        @Override
+        protected Bitmap doInBackground(String... strings) {
+            URL url = null;
+            HttpURLConnection urlConnection = null;
 
+            try {
+                url = new URL(strings[0]);
+                urlConnection = (HttpURLConnection) url.openConnection();
+                InputStream inputStream = urlConnection.getInputStream();
+                Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
+                return bitmap;
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                if (urlConnection != null){
+                    urlConnection.disconnect();
+                }
+            }
+            return null;
+        }
+    }
 }
